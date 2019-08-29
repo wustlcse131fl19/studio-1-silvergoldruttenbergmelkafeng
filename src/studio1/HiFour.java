e6d75dfd144d069ff9e0b96c3b2c2ff634f22dce
Kevin Silvergold <ksilvergold@wustl.edu>
2019-08-29 13:38:07 -0500

    silvergoldruttenbergmelkafeng group
    Studio 1

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,10 +9,11 @@ import support.cse131.ArgsProcessor;
 public class HiFour {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		String s0 = ap.nextString("Enter value for arg 0");
-		String s1 = ap.nextString("Enter value for arg 1");
-		String s2 = ap.nextString("Enter value for arg 2");
-		String s3 = ap.nextString("Enter value for arg 3");
+		String s0 = ap.nextString("Please enter the first name:");
+		String s1 = ap.nextString("Please enter the second name:");
+		String s2 = ap.nextString("Please enter the third name:");
+		String s3 = ap.nextString("Please enter the fourth name:");
+		System.out.print("Greetings " + s0 + ", " + s1 + ", " + s2 + ", and " + s3 + ".");
 		//
 		// Say hello to the names in s0 through s3.
 		//
